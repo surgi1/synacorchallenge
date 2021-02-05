74c7f0eeddaaf436667a354edb1ed252e1827210
Jan Surovec <gh1@cloudia.cz>
2021-02-05 21:33:30 +0100

    formatting

--- read.js
+++ read.js
@@ -1,13 +1,12 @@
-var fs = require('fs');
+let fs = require('fs');
 
 function getByteArray(filePath){
     let fileData = fs.readFileSync(filePath).toString('hex');
-    let result = []
-    for (var i = 0; i < fileData.length; i+=4)
-      result.push(parseInt('0x'+fileData[i+2]+''+fileData[i+3]+fileData[i]+''+fileData[i+1]))
+    let result = [];
+    for (let i = 0; i < fileData.length; i+=4) result.push(parseInt('0x'+fileData[i+2]+''+fileData[i+3]+fileData[i]+''+fileData[i+1]));
     return result;
 }
 
-result = getByteArray('challenge.bin')
-console.log(result)
+let result = getByteArray('challenge.bin');
+console.log(result);
 fs.writeFileSync('input.js', 'var input = ['+result.join(",")+']');

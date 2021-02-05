ceb2e44f90f76ed16aa8aa1f5f18f3e81225f70b
Jan Surovec <gh1@cloudia.cz>
2021-02-05 18:56:35 +0100

    formatting, also added digest for teleport solver

--- teleporter.js
+++ teleporter.js
@@ -1,27 +1,39 @@
 /*
-0x156b:  set R0 0x0004
-0x156e:  set R1 0x0001
-0x1571: call 0x178b
-0x1573:   eq R1 R0 0x0006
+156b:  set R0 0004
+156e:  set R1 0001
+1571: call 178b
+1573:   eq R1 R0 0006
 
 ...
 
-0x178b:   jt R0 0x1793
-0x178e:  add R0 R1 0x0001
-0x1792:  ret 
-0x1793:   jt R1 0x17a0
-0x1796:  add R0 R0 0x7fff
-0x179a:  set R1 R7
-0x179d: call 0x178b
-0x179f:  ret 
-0x17a0: push R0
-0x17a2:  add R1 R1 0x7fff
-0x17a6: call 0x178b
-0x17a8:  set R1 R0
-0x17ab:  pop R0
-0x17ad:  add R0 R0 0x7fff
-0x17b1: call 0x178b
-0x17b3:  ret 
+178b:   jt R0 1793
+178e:  add R0 R1 0001
+1792:  ret 
+1793:   jt R1 17a0
+1796:  add R0 R0 7fff
+179a:  set R1 R7
+179d: call 178b
+179f:  ret 
+17a0: push R0
+17a2:  add R1 R1 7fff
+17a6: call 178b
+17a8:  set R1 R0
+17ab:  pop R0
+17ad:  add R0 R0 7fff
+17b1: call 178b
+17b3:  ret 
+*/
+
+/*
+// one of the middle steps
+let r0 = 4, r1 = 1, r7 = 123
+const main = (a, b) => {
+    if (a == 0) return (b+1) % 0x8000;
+    if (b == 0) return main( (a-1) % 0x8000, r7 );
+    return main((a - 1) % 0x8000, main(a, (b - 1) % 0x8000));
+}
+
+main(r0, r1);
 */
 
 /*
@@ -49,7 +61,8 @@ const main = () => {
 }
 */
 
-// after numerous hours spent on snalyzing this !@&^%@, attempts on rewrites and partial tests and more rewrites.. and more &*@#%&^% ..
+// after numerous hours spent on analyzing this !@&^%@, attempts on rewrites and partial tests and more rewrites.. and more &*@#%&^% ..
+// also, see https://en.wikipedia.org/wiki/Ackermann_function
 
 const loop = (n, r7) => {
     let a = powMod(r7+1, n, 0x8000),

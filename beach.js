43d924c3c78ac26d98be31b2268e5b8426c1dc7b
Jan Surovec <gh1@cloudia.cz>
2021-02-18 17:36:25 +0100

    readability changes, minor

--- beach.js
+++ beach.js
@@ -11,24 +11,24 @@ let start = {x:0, y:3}, end = {x:3, y:0}, maxSteps = 15, i = 0,
     paths = [{x: start.x, y: start.y, val: 0, op: '+', steps: []}];
 
 while (paths[i]) {
-    let p = paths[i++], val = map[p.y][p.x];
+    let p = paths[i++], val = map[p.y][p.x], expand = true;
 
     if (isNaN(val)) p.op = val;
     else if (p.op == '+') p.val += val;
     else if (p.op == '-') p.val -= val;
     else if (p.op == '*') p.val *= val;
 
-    if (p.val < -20 || p.val > 120) p.done = true;
-    if (p.x == start.x && p.y == start.y && p.steps[0]) p.done = true;
+    if (p.val < -20 || p.val > 120) expand = false;
+    if (p.x == start.x && p.y == start.y && p.steps[0]) expand = false;
     if (p.x == end.x && p.y == end.y) {
-        p.done = true;
+        expand = false;
         if (p.val == 30 && p.steps.length < maxSteps) {
             console.log('shortest trip found', p.steps, p);
             maxSteps = p.steps.length;
         }
     }
 
-    if (!p.done) directions.map(d => {
+    if (expand) directions.map(d => {
         let np = {x: p.x+d.x, y: p.y+d.y, val: p.val, op: p.op, steps: [...p.steps, d.v]}
         if (map[np.y] && map[np.y][np.x] && np.steps.length < maxSteps) paths.push(np);
     })
